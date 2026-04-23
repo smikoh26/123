a3c1539671f76f6a9acff18f0f0763f11172c0a0
smikoh26 <jimmywangdio26@gmail.com>
2026-04-23 05:18:28 +0000

    Connect project to remote Git repository.

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Docker Flask!"
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1><p>This is the about page.</p><a href='/'>Back to Home</a>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(
+        host="0.0.0.0", 
+        port=5000,
+        debug=True
+    )
+    
+
